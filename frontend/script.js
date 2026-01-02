799d2ce353949c4468640d4d0c88f805b4d06fed
sekharchandana <vara3330@gmail.com>
2026-01-02 20:43:59 +0530

    updated backend url

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api/tasks';
+const API_URL = 'https://deploying-backend.onrender.com/data';
 
 // DOM Elements
 const taskForm = document.getElementById('taskForm');
